3c5d455bf12cf3761abaa67883f0181bf2eab1cb
jwalke23 <jaw6.mail@gmail.com>
2024-01-25 12:33:56 -0500

    Added dotfiles

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.enable_tab_bar = false
+
+config.color_scheme = 'Catppuccin Latte'
+
+config.colors = {
+	background = 'rgba(239, 241, 245, 0.4)'
+}
+
+return config
